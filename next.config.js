37bc577d07c476ebc8fc6066535b5f1433a71744
Anish Shanbhag <anish.shanbhag@yahoo.com>
2024-09-24 04:32:12 -0400

    Add redirect to documentation project

--- next.config.js
+++ next.config.js
@@ -10,4 +10,13 @@ module.exports = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/synapse",
+        destination: "https://terrific-friction-15e.notion.site/Documentation-reimagined-62d8db233e9644589bfe3a30e4556708",
+        permanent: true,
+      },
+    ]
+  },
 };
